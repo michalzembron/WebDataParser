2f01c786495c6c7aec555fdf38d6db8130376d38
Michał Zembroń <michalzembron@gmail.com>
2020-12-08 10:01:10 +0100

    First commit

---
+++ WebDataParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebDataParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
