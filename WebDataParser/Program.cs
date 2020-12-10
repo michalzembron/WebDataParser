5147c2de148d08f8eb66dd2bde445c806dd5e2bc
Michał Zembroń <michalzembron@gmail.com>
2020-12-10 15:05:31 +0100

    Saving output to a text file
    path to a file: project root
    name: Output.txt

--- WebDataParser/Program.cs
+++ WebDataParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using RestSharp;
 
 namespace WebDataParser
@@ -7,11 +8,19 @@ namespace WebDataParser
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("https://api.covid19api.com/");
+            var client = new RestClient("https://api.covid19api.com/total/country/poland");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
+            string responseContent = response.Content;
+            Console.WriteLine(responseContent);
+
+            string dataToBeSaved = responseContent;
+            dataToBeSaved = dataToBeSaved.Replace("},", "},\n");
+            string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Output.txt");
+            destPath = destPath.Replace("WebDataParser\\bin\\Debug\\netcoreapp3.1\\", "");
+
+            File.WriteAllText(destPath, dataToBeSaved.ToString());
         }
     }
 }
