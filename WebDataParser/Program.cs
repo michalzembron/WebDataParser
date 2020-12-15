ef0cdb7647a91eb0470ab58369fff5e211658ec4
Michał Zembroń <michalzembron@gmail.com>
2020-12-15 11:12:39 +0100

    Added more data sources

--- WebDataParser/Program.cs
+++ WebDataParser/Program.cs
@@ -8,7 +8,10 @@ namespace WebDataParser
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("https://api.covid19api.com/total/country/poland");
+            //var client = new RestClient("https://api.covid19api.com/total/country/poland");
+            //var client = new RestClient("https://api.covid19api.com/world/total");
+            //var client = new RestClient("https://api.covid19api.com/dayone/country/poland/status/confirmed/live");
+            var client = new RestClient("https://api.covid19api.com/summary");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
